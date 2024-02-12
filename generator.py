fb597e1869e0527f7c4c6a7e85463724e9716c04
Chin Ling Ng <nchinling@Chins-MacBook-Pro.local>
2024-02-12 22:24:53 +0800

    weighted next word

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 import random
 from db_utlilty import get_sentences_from_database
+from collections import Counter
 
 
 def choose_next_word(last_word):
@@ -9,13 +10,24 @@ def choose_next_word(last_word):
     global sentences
     sentences = get_sentences_from_database()
     candidate_words = []
+    word_frequency = Counter()
+
     for sentence in sentences:
         words = sentence.split()
         for i in range(len(words) - 1):
             if words[i] == last_word:
                 candidate_words.append(words[i + 1])
+                word_frequency[words[i + 1]] += 1
                 print(candidate_words)
     if candidate_words:
-        return random.choice(candidate_words)
+        # return random.choice(candidate_words)
+        # Weighted selection based on frequency
+        total_frequency = sum(word_frequency.values())
+        # create dictionary (dictionary comprehension)
+        probabilities = {word: freq / total_frequency for word,
+                         freq in word_frequency.items()}
+        chosen_word = random.choices(
+            list(probabilities.keys()), weights=list(probabilities.values()))[0]
+        return chosen_word
     else:
         return None
